89f16e940fddfdec44a424d704998366b15755dc
Jorgeimlz <jorge.moncayo.loaiza@udla.edu.ec>
2024-11-29 12:59:04 -0500

    estaticos plisss

--- RecomendacionesNutri/settings.py
+++ RecomendacionesNutri/settings.py
@@ -171,15 +171,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
+STATIC_ROOT = "staticfiles"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Carpeta donde se recopilarán los archivos estáticos
 
 # Configuración adicional para producción
-if not DEBUG:
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
