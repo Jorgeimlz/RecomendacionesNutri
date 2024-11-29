629801a7a963725e01c62277e9998f4f8f65b787
Jorgeimlz <jorge.moncayo.loaiza@udla.edu.ec>
2024-11-29 14:06:59 -0500

    dasdad

--- RecomendacionesNutri/settings.py
+++ RecomendacionesNutri/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gy&c^^g5m%!rh)b8!+^=!4#@-x#eu@$x64ctf11h+lcgs4=(!('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'recomendacionesnutri.onrender.com']
 
